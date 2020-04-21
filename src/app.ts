353321752f211e9af1f25d9ee19145c902b531ad
fs-projects <fs.projectsuk@gmail.com>
2020-04-21 16:42:15 +0530

    cleanup extra code-start refer here

--- src/app.ts
+++ src/app.ts
@@ -111,31 +111,6 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor){
   return adjDescriptor;
 }
 
-//Component Base Class 
-class Component <T extends HTMLElement, U extends HTMLElement>{
-  templateElement: HTMLTemplateElement;
-  hostElement: T;
-  element: U;
-
-  constructor(templateId: string, hostElementId: string, insertAtStart: boolean, newElementId?: string){
-    this.templateElement = document.getElementById(templateId)! as HTMLTemplateElement;
-    this.hostElement = document.getElementById(hostElementId)! as T;
-
-    const importedNode = document.importNode(this.templateElement.content, true);
-    this.element = importedNode.firstElementChild as U;  
-    if(newElementId){
-      this.element.id = newElementId;
-    }
-    this.attach(insertAtStart);
-  }
-
-   //ProjectList rendering function -
-   private attach(insertAtBeginning: boolean){
-      this.hostElement.insertAdjacentElement(insertAtBeginning ? 'afterbegin' : 'beforeend', this.element);
-  }
-
-}
-
 //ProjectList class
 class ProjectList {
 
